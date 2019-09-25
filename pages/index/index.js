d1871d3b42d0094d8f754c0bfd67b5c131ce7a82
wangxingxiang <wangxingxiang@111.com.cn>
2019-09-25 18:24:32 +0800

    end

--- pages/index/index.js
+++ pages/index/index.js
@@ -7,57 +7,11 @@ Page({
   data: {
     image: '/assets/placeholder.jpg',
     showTips: false,
-    info:null,
-    result: null,
-    sign: null
+    result: null
   },
 
   /**
-   * 分析照片
-   */
-  detectImage(imgbase64) {
-    const that = this
-
-    // 取消之前的结果显示
-    that.setData({ result: null })
-
-    // loading
-    wx.showLoading({ title: '分析中...' })
-    var params = {};
-    params.appid = 2122110749;
-    params.time_stamp = (Date.now() / 1000).toFixed();
-    params.nonce_str = Math.random().toString(36).substr(2);
-    params.sign = ''
-    params.image = imgbase64
-    //let sign = this.getReqSign(params,'SFFaDRax3Q8MxNLc')
-    let sign = utils.signedParam(params)
-    params.sign = sign
-    // 将图片上传至 AI 服务端点
-    wx.request({
-      url: 'https://api.ai.qq.com/fcgi-bin/ptu/ptu_faceage',
-      data: params,
-      method: "POST",
-      success (res) {
-        // 解析 JSON
-        console.log(res)
-        const result = res.data
-
-        if (result.ret === 0) {
-          // 成功获取分析结果
-          that.setData({ result: result.data.face[0] })
-        } else {
-          // 检测失败
-          wx.showToast({ icon: 'none', title: '找不到你的小脸蛋喽～' })
-        }
-
-        // end loading
-        wx.hideLoading()
-      }
-    })
-  },
-
-  /**
-   * 获取照片
+   * 获取照片，分析照片
    */
   getImage (type = 'camera') {
     const that = this
@@ -150,47 +104,6 @@ Page({
     return { title: `刚刚测了我的颜值「${this.data.result.beauty}」你也赶紧来试试吧！` }
   },
 
-  getReqSign(params, appkey) {
-    /**
-     *  app_id	10000	应用标识
-        time_stamp	1493449657	秒级时间戳
-        nonce_str	20e3408a79	随机字符串
-        key1	腾讯AI开放平台	接口请求数据，本示例为UTF-8编码
-        key2	示例仅供参考	接口请求数据，本示例为UTF-8编码
-        sign		接口请求签名，待计算
-     */
-    // 1. 字典升序排序
-    let paramsJson = JSON.stringify(params)
-    let sortParams = JSON.parse(paramsJson)
-    console.log(sortParams)
-
-    // 2. 拼按URL键值对
-    var str = this.jsonSort(sortParams)
-    
-    // 3. 拼接app_key
-    str = str + 'app_key=' + appkey
-    //console.log(str)
-    // 4. MD5运算+转换大写，得到请求签名
-    var md5 = require('../../md5-1.js')
-    let md5str = md5.hex_md5(str).toUpperCase()
-    console.log(md5str)
-    return md5str
-  },
-
-  jsonSort: function (jsonObj) {
-    let arr = [];
-    for (var key in jsonObj) {
-      arr.push(key)
-    }
-    arr.sort();
-    let str = '';
-    for (var i in arr) {
-      if (jsonObj[arr[i]]!=''){
-        str += arr[i] + "=" + jsonObj[arr[i]] + "&"
-      }
-    }
-    return str.substr(0, str.length - 1)
-  },
   scan(image) {
     let params = {
       image: image,
